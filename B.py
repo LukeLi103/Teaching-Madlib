b2cfe93a6596036d58db183743760daa2c9717c1
Luke Li <lukeawesome777@gmail.com>
2023-09-26 21:45:58 -0400

    cool

--- B.py
+++ B.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-number_between_1_and_9000 = 0 # assign some number or string at the end of each of these lines
-adjective_1 = ""
-noun_1 = ""
-famous_person = ""
-adjective_2 = ""
-very_large_number = 0
-noun_2 = ""
-adjective_2 = ""
-large_store = ""
+number_between_1_and_9000 = 5812 # assign some number or string at the end of each of these lines
+adjective_1 = "blazing"
+noun_1 = "alien"
+famous_person = "barack obama"
+adjective_2 = "chunky"
+very_large_number = 98789
+noun_2 = "pole"
+adjective_2 = "tasty"
+large_store = "dunkin"
